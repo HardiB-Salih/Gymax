7e4a925de51248f753b5ec3841e4b546f58a30d1
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2024-01-19 02:06:20 +0300

    inital commit

---
+++ src/pages/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+import { Box, Center, Spinner, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const LoadingScreen = () => {
+  return (
+    <Center>
+      <Text> Loading...</Text>
+    </Center>
+  );
+};
+
+export default LoadingScreen;
